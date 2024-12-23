f90be16b595d9540b3b3768b85cadfda24a01a4b
Isulew <netcookies@gmail.com>
2024-12-23 12:28:36 +0800

    update api

--- src/gewe-notify-card.js
+++ src/gewe-notify-card.js
@@ -118,9 +118,8 @@ class GeweNotifyCard extends LitElement {
     this.hass.callApi('GET', 'gewe_contacts')
       .then(result => {
         console.log('API call result:', result);
-        console.log('API call result attributes:', result.attributes);
-        this.friends = result.attributes.friends || [];
-        this.chatrooms = result.attributes.chatrooms || [];
+        this.friends = result.friends || [];
+        this.chatrooms = result.chatrooms || [];
         this.filterData();
       })
       .catch(error => {
